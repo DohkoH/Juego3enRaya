8b58a8325dbaef643dfeabd49da0d604d90bdeac
DohkoH <agumonxdx@gmail.com>
2021-05-05 12:35:22 -0500

    05/05/2021-Interface y Boton

--- MachineLearning.py
+++ MachineLearning.py
@@ -1,22 +1,44 @@
 import tkinter as tk
+from PIL import Image ,ImageTk
+
+def DibujarX(x,y):
+
+    global Frame2
+
+    X=Image.open('X.png')
+    X=ImageTk.PhotoImage(X)
+    X=tk.Label(Frame2,image=X)
+    X.grid(row=x,column=y)
+
+
 
 Ventana=tk.Tk()
 Ventana.title("Proyecto Concurso")
-Ventana.geometry("600x600")
+Ventana.geometry("550x700")
 Ventana.resizable(0,0)
 
 Frame1=tk.Frame(Ventana)
-Frame1.pack()
+Frame1.grid(row=0,column=0)
 
 Frame2=tk.Frame(Ventana)
-Frame2.pack()
+Frame2.grid(row=1,column=0)
+
+Frame3=tk.Frame(Ventana)
+Frame3.grid(row=2,column=0)
+#Titulo
 
 Texto=tk.Label(Frame1,text="Juego Tres en Raya")
-Texto.grid(row=0,column=0)
+Texto.grid(row=0,column=0,columnspan=3)
+
+#Fondo
 
 Foto=tk.PhotoImage(file="MiChi.png")
-Foto.subsample(4)
 Imagen=tk.Label(Frame2,image=Foto)
-Imagen.grid(row=0,column=0)
+Imagen.grid(row=0,column=0,columnspan=3)
+
+#Botones
+
+BotonX = tk.Button(Frame3,text="X",command= lambda: DibujarX(0,0))
+BotonX.pack()
 
 Ventana.mainloop()
